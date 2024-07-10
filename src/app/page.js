cb5ef697a0de02c85422ad28bf108a9993d8babe
Ammarahmad786 <91366905+Ammarahmad786@users.noreply.github.com>
2024-07-10 22:38:35 +0500

    Styles and scss files settle down working

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,14 @@
 'use client'
 import React from 'react'
 import "@/app/styles/flex.css"
-// import "@/app/styles/scss/_common.scss"
+
 import "@/app/styles/global/style.css"
 import "@/app/styles/global/plugins/bootstrap-grid.css"
 // import "@/app/styles/global/plugins/font-awesome.min.css"
 import "@/app/styles/global/plugins/swiper.min.css"
+
+import "@/app/styles/scss/style.scss"
+
 import Logo from './components/sections/logoSlider/Logo'
 import Divider from './components/common/Divider'
 const page = () => {
