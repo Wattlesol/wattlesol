ffb4cd9ef7f5f2454f246fb1af1b1181311acd10
Ammarahmad786 <91366905+Ammarahmad786@users.noreply.github.com>
2024-07-10 23:14:37 +0500

    portfolio

---
+++ src/app/components/sections/LatestProjects/LatestProjects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LatestProjects = () => {
+  return (
+    <section class="mil-works mil-p-120-90">
+            <div class="mil-deco" style="top: 0; right: 40%;"></div>
+            </section>
+  )
+}
+
+export default LatestProjects
