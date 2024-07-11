1212b00016f458b578c4466e5944b031435f1bee
Ammarahmad786 <91366905+Ammarahmad786@users.noreply.github.com>
2024-07-11 05:31:53 +0500

    FAQ SECTION COMPLETED WORKING

---
+++ src/app/components/sections/Faqs/Main/Main.jsx
@@ -0,0 +1,204 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const Main = () => {
+    return (
+        <section class="mil-faqs  mil-p-120-0">
+            <div class="mil-deco" style={{ bottom: 0, left: "10%", transform: "rotate(180deg)" }}></div>
+            <Container>
+                <div class="mil-tabs">
+                    <input type="radio" name="tabs" id="tab1" checked="checked" />
+                    <label for="tab1">Information</label>
+                    <div class="mil-tab">
+                        <Row className='justify-content-between'>
+                            <Col lg={4}>
+                                <h3 class="mil-up-font mil-mb-30"><span class="mil-accent">ITSulu Connects</span> The Entire Organization From End To End.</h3>
+                                <p class="mil-mb-60">Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius.</p></Col>
+                                <Col lg={7}>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                </Col>
+
+                        </Row>
+
+
+                    </div>
+                    <input type="radio" name="tabs" id="tab1" checked="checked" />
+                    <label for="tab1">Information</label>
+                    <div class="mil-tab">
+                        <Row className='justify-content-between'>
+                            <Col lg={4}>
+                                <h3 class="mil-up-font mil-mb-30"><span class="mil-accent">ITSulu Connects</span> The Entire Organization From End To End.</h3>
+                                <p class="mil-mb-60">Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius.</p></Col>
+                                <Col lg={7}>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                </Col>
+
+                        </Row>
+
+
+                    </div>
+                    <input type="radio" name="tabs" id="tab1" checked="checked" />
+                    <label for="tab1">Information</label>
+                    <div class="mil-tab">
+                        <Row className='justify-content-between'>
+                            <Col lg={4}>
+                                <h3 class="mil-up-font mil-mb-30"><span class="mil-accent">ITSulu Connects</span> The Entire Organization From End To End.</h3>
+                                <p class="mil-mb-60">Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius.</p></Col>
+                                <Col lg={7}>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                </Col>
+
+                        </Row>
+
+
+                    </div>
+                    <input type="radio" name="tabs" id="tab1" checked="checked" />
+                    <label for="tab1">Information</label>
+                    <div class="mil-tab">
+                        <Row className='justify-content-between'>
+                            <Col lg={4}>
+                                <h3 class="mil-up-font mil-mb-30"><span class="mil-accent">ITSulu Connects</span> The Entire Organization From End To End.</h3>
+                                <p class="mil-mb-60">Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius.</p></Col>
+                                <Col lg={7}>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                <div class="mil-accordion">
+                                    <h6>Information about ITSulu</h6>
+                                </div>
+                                <div class="mil-panel">
+                                    <div class="mil-window">
+                                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures.</p>
+                                    </div>
+                                </div>
+                                </Col>
+
+                        </Row>
+
+
+                    </div>
+                </div>
+
+
+            </Container>
+
+
+        </section>
+    )
+}
+
+export default Main
