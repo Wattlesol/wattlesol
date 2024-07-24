712c82af9829e02df2d9392b2088166b7777175c
Awais2512 <hafiz.abdullah641@gmail.com>
2024-07-23 19:06:50 -0700

    added team images

--- src/app/components/sections/TeamPageSections/Main/Main.jsx
+++ src/app/components/sections/TeamPageSections/Main/Main.jsx
@@ -2,6 +2,14 @@ import Image from 'next/image';
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import FaceImage1 from '@/app/public/global/img/faces/abdullah.jpg';
+import Zain from '@/app/public/global/img/faces/zain.jpg';
+import Saima from '@/app/public/global/img/faces/saima.jpg';
+import Ammar from '@/app/public/global/img/faces/ammar.jpg';
+import Azaan from '@/app/public/global/img/faces/azaan.jpg';
+import Muiz from '@/app/public/global/img/faces/muiz.jpg';
+import Shayan from '@/app/public/global/img/faces/shayan.jpg';
+import Sheeraz from '@/app/public/global/img/faces/sheeraz.jpg';
+import Zaiqam from '@/app/public/global/img/faces/zaigam.jpg';
 const Main = () => {
   return (
     <section className="mil-team mil-p-120-60">
@@ -18,7 +26,7 @@ const Main = () => {
           <Col sm={6} lg={4}>
             <a href="team-single.html" className="mil-team-card mil-mb-60">
               <div className="mil-image-frame mil-mb-30">
-                <Image src={FaceImage1} alt="Team member" />
+                <Image src={Zain} alt="Team member" />
                 <div className="mil-team-circle"></div>
               </div>
               <h4 className="mil-mb-10">Syed Zain</h4>
@@ -28,27 +36,27 @@ const Main = () => {
           <Col sm={6} lg={4}>
             <a href="team-single.html" className="mil-team-card mil-mb-60">
               <div className="mil-image-frame mil-mb-30">
-                <Image src={FaceImage1} alt="Team member" />
+                <Image src={Sheeraz} alt="Team member" />
                 <div className="mil-team-circle"></div>
               </div>
-              <h4 className="mil-mb-10">M Sheraz</h4>
+              <h4 className="mil-mb-10">Muhammad Sheraz</h4>
               <p>Lead Software Developer</p>
             </a>
           </Col>
           <Col sm={6} lg={4}>
             <a href="team-single.html" className="mil-team-card mil-mb-60">
               <div className="mil-image-frame mil-mb-30">
-                <Image src={FaceImage1} alt="Team member" />
+                <Image src={Zaiqam} alt="Team member" />
                 <div className="mil-team-circle"></div>
               </div>
-              <h4 className="mil-mb-10">Syed Zaigham</h4>
-              <p>Senior UI/UX Designer</p>
+              <h4 className="mil-mb-10">Zaigham Bilal</h4>
+              <p>Lead UI/UX Designer</p>
             </a>
           </Col>
           <Col sm={6} lg={4}>
             <a href="team-single.html" className="mil-team-card mil-mb-60">
               <div className="mil-image-frame mil-mb-30">
-                <Image src={FaceImage1} alt="Team member" />
+                <Image src={Saima} alt="Team member" />
                 <div className="mil-team-circle"></div>
               </div>
               <h4 className="mil-mb-10">Saima Yasmeen</h4>
@@ -58,17 +66,17 @@ const Main = () => {
           <Col sm={6} lg={4}>
             <a href="team-single.html" className="mil-team-card mil-mb-60">
               <div className="mil-image-frame mil-mb-30">
-                <Image src={FaceImage1} alt="Team member" />
+                <Image src={Shayan} alt="Team member" />
                 <div className="mil-team-circle"></div>
               </div>
-              <h4 className="mil-mb-10">Hussain Mukhtar</h4>
-              <p>GFX Desginer</p>
+              <h4 className="mil-mb-10">Syed Shayan Bukhari</h4>
+              <p>UI\UX | GFX</p>
             </a>
           </Col>
           <Col sm={6} lg={4}>
             <a href="team-single.html" className="mil-team-card mil-mb-60">
               <div className="mil-image-frame mil-mb-30">
-                <Image src={FaceImage1} alt="Team member" />
+                <Image src={Muiz} alt="Team member" />
                 <div className="mil-team-circle"></div>
               </div>
               <h4 className="mil-mb-10">Muizz Ali</h4>
@@ -90,7 +98,7 @@ const Main = () => {
           <Col sm={6} lg={4}>
             <a href="team-single.html" className="mil-team-card mil-mb-60">
               <div className="mil-image-frame mil-mb-30">
-                <Image src={FaceImage1} alt="Team member" />
+                <Image src={Ammar} alt="Team member" />
                 <div className="mil-team-circle"></div>
               </div>
               <h4 className="mil-mb-10">Ammar</h4>
@@ -101,7 +109,7 @@ const Main = () => {
           <Col sm={6} lg={4}>
             <a href="team-single.html" className="mil-team-card mil-mb-60">
               <div className="mil-image-frame mil-mb-30">
-                <Image src={FaceImage1} alt="Team member" />
+                <Image src={Azaan} alt="Team member" />
                 <div className="mil-team-circle"></div>
               </div>
               <h4 className="mil-mb-10">Azaan Ahmed</h4>
